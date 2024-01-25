505a33cd71799c1abb4c883d4c636b658b34c9b3
Zeeshan Mukhtar <zeshanmukhtar878@gmail.com>
2024-01-25 15:54:24 +0500

    👽️ Add user routes to app.js

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,7 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const registerUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "user is registered succefully..!" });
+});
+
+export { registerUser };
