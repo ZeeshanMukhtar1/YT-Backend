1c732a7e80366850433d12e5d25140fe4eba3a97
Zeeshan Mukhtar <zeshanmukhtar878@gmail.com>
2024-01-26 14:14:53 +0500

    🐛 fix user registration error

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -43,7 +43,14 @@ const registerUser = asyncHandler(async (req, res) => {
 
   // check for images, check for avatar
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
 
   // check for avatar validation
   if (!avatarLocalPath) {
